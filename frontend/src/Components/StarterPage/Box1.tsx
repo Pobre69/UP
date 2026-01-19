59c568270c4d20e79923b66c58d4851143d1f214
Guilherme <guiventurini25@gmail.com>
2026-01-19 11:44:04 -0300

    Co-authored-by: ultramegaalguem <ultramegaalguem@users.noreply.github.com>

--- frontend/src/Components/StarterPage/Box1.tsx
+++ frontend/src/Components/StarterPage/Box1.tsx
@@ -3,12 +3,20 @@ import "../../Design/StarterPage/Box1.css";
 export default function Box1() {
     return (
         <div id="box1">
-            <div>
-                <p className="White">Elevando o Seu <br />Negócio</p>
-                <div></div>
+            <div id="box1_part1">
+                <div id="Card"><span id="Ponto"></span> Transformação Digital</div>
+                <p className="text_White">Elevando o Seu <br /><span className="text_Purple">Negócio</span></p>
+                <h4 className="text_Gray">texto gigante</h4>
+                <button id="SaibaMaisButton">Saiba Mais</button>
+                <div>
+                    <div><span className="box1_textEdit2">500+</span><br /><span className="box1_textEdit">Clientes</span></div>
+                    <div><span className="box1_textEdit2">98%</span><br /><span className="box1_textEdit">Satisfação</span></div>
+                    <div><span className="box1_textEdit2">5 anos</span><br /><span className="box1_textEdit">Experiência</span></div>
+                </div>
             </div>
-            <div>
-                penis
+            <div id="box1_part2">
+                <img id="box1_img" src="../../Images/" alt="Imagem de Gestão" />
+                <div id="box1_img_Effect"></div>
             </div>
         </div>
     )
--- frontend/src/Pages/StarterPage.tsx
+++ frontend/src/Pages/StarterPage.tsx
@@ -1,12 +1,14 @@
 import Header from "../Components/StarterPage/Header";
 import Box1 from "../Components/StarterPage/Box1";
+import Box2 from "../Components/StarterPage/Box2";
 import "../Design/StarterPage/StarterPage.css";
 
-export default function StartPage() {
+export default function StarterPage() {
     return (
         <>
-            <Header />
+            <Header /> 
             <Box1 />
+            <Box2 />
         </>
     )
 }
