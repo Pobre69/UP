45ddc7f2603101ec8cedbcd77a4f2527c4533fe4
Pobre69 <versos2007@gmail.com>
2026-01-19 23:48:04 -0300

    react vite 1.3.0 ("Box2")

--- frontend/src/Components/StarterPage/Box1.tsx
+++ frontend/src/Components/StarterPage/Box1.tsx
@@ -4,11 +4,15 @@ export default function Box1() {
     return (
         <div id="box1">
             <div id="box1_part1">
-                <div id="Card"><span id="Ponto"></span> Transformação Digital</div>
+                <div id="Card"><span id="Ponto"></span>Transformação Digital</div>
                 <p className="text_White">Elevando o Seu <br /><span className="text_Purple">Negócio</span></p>
-                <h4 className="text_Gray">texto gigante</h4>
-                <button id="SaibaMaisButton">Saiba Mais</button>
-                <div>
+                <h4 className="text_Gray">Potencialize seus resultados com soluções digitais inovadoras. Juntos, construímos o futuro do seu negócio.</h4>
+                <div id="SaibaMaisButtonEffects">
+                    <div className="glow left"></div>
+                    <div className="glow right"></div>
+                    <button id="SaibaMaisButton">Saiba Mais</button>
+                </div>
+                <div id="box1_textDisplay">
                     <div><span className="box1_textEdit2">500+</span><br /><span className="box1_textEdit">Clientes</span></div>
                     <div><span className="box1_textEdit2">98%</span><br /><span className="box1_textEdit">Satisfação</span></div>
                     <div><span className="box1_textEdit2">5 anos</span><br /><span className="box1_textEdit">Experiência</span></div>
--- frontend/src/Components/StarterPage/Box2.tsx
+++ frontend/src/Components/StarterPage/Box2.tsx
@@ -3,45 +3,45 @@ import "../../Design/StarterPage/Box2.css";
 export default function Box2() {
     return (
         
-        <div>
+        <div id="box2">
             <div id="Title-ComoTrabalhamos">
                 <h3> 
-                    Como trabalhamos
+                    Como <span id="title_Recolor">trabalhamos</span>
                     <br />
                     <span className="TitlePhrase">Um processo simples e eficiente para transformar sua presença digital</span>
                 </h3>
             </div>
             <div id="BoxRow">
-                <div id="BoxContainer">
-                    <div id="BoxContent">
-                        <div id="iconBox">
+                <div className="BoxContainer">
+                    <div className="BoxContent">
+                        <div className="iconBox">
                             1
                         </div>
                         <p className="ptitle">Consultoria & Diagnóstico</p>
                         <p className="ptext">Entendemos o seu negócio, analisamos seu Instagram, público e objetivos. Nada é genérico, tudo começa com estratégia.</p>
                     </div>
                 </div>
-                <div id="BoxContainer">
-                    <div id="BoxContent">
-                        <div id="iconBox">
+                <div className="BoxContainer">
+                    <div className="BoxContent">
+                        <div className="iconBox">
                             2
                         </div>
                         <p className="ptitle">Planejamento Estratégico</p>
                         <p className="ptext">Definimos identidade visual, tipo de conteúdo, frequência de posts e tom de comunicação. Aqui nasce o plano de crescimento.</p>
                     </div>
                 </div>                   
-                <div id="BoxContainer">
-                    <div id="BoxContent">
-                        <div id="iconBox">
+                <div className="BoxContainer">
+                    <div className="BoxContent">
+                        <div className="iconBox">
                             3
                         </div>
                         <p className="ptitle">Criação & Publicação</p>
                         <p className="ptext">Criamos posts e stories profissionais e publicamos nos melhores horários, mantendo o perfil organizado e ativo.</p>
                     </div>
                 </div>
-                <div id="BoxContainer">
-                    <div id="BoxContent">
-                        <div id="iconBox">
+                <div className="BoxContainer">
+                    <div className="BoxContent">
+                        <div className="iconBox">
                             4
                         </div>
                         <p className="ptitle">Acompanhamento Contínuo</p>
